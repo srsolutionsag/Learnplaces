6b778a495e6b60d581eb1dc8366be44cbd574dc5
Nicolas Schäfli <ns@studer-raimann.ch>
2017-10-13 13:56:34 +0200

    Add plugin php

---
+++ plugin.php
@@ -0,0 +1,7 @@
+<?php
+$id = "srlp";
+$version = "0.0.1";
+$ilias_min_version = "5.3.0";
+$ilias_max_version = "5.3.999";
+$responsible = "Fabian Schmid";
+$responsible_mail = "fs@studer-raimann.ch";
