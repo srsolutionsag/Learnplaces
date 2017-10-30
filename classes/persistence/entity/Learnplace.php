f51a3115d2c3bac5e8376eacb65822916c50b97f
Nicolas Schäfli <ns@studer-raimann.ch>
2017-10-30 17:52:28 +0100

    Add unique constraint to Learnplace entity

--- classes/persistence/entity/Learnplace.php
+++ classes/persistence/entity/Learnplace.php
@@ -45,6 +45,7 @@ class Learnplace extends ActiveRecord {
 	 * @var int
 	 *
 	 * @con_has_field  true
+	 * @con_is_unique  true
 	 * @con_is_notnull true
 	 * @con_fieldtype  integer
 	 * @con_length     8
