b72db6f84a4788536ede32c2012e194c65f9d9fd
Nicolas Schäfli <ns@studer-raimann.ch>
2017-10-31 13:01:00 +0100

    Add missing picture upload block

---
+++ classes/persistence/dto/PictureUploadBlock.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace SRAG\Learnplaces\persistence\dto;
+
+use SRAG\Lernplaces\persistence\dto\Block;
+
+/**
+ * Class PictureUploadBlock
+ *
+ * @package SRAG\Learnplaces\persistence\dto
+ *
+ * @author  Nicolas Schäfli <ns@studer-raimann.ch>
+ */
+class PictureUploadBlock extends Block {
+
+}
