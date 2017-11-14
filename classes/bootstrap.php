26972631faec9442a9997982ba2d580b9448d33e
Nicolas Schäfli <ns@studer-raimann.ch>
2017-11-14 11:36:46 +0100

    Removed aop bootstrap code

--- classes/bootstrap.php
+++ classes/bootstrap.php
@@ -3,6 +3,7 @@
 use SRAG\Learnplaces\aop\aspect\LearnplaceAspectKernel;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+/*
 LearnplaceAspectKernel::getInstance()
 	->init([
 		'debug' => true, // use 'false' for production mode
@@ -13,3 +14,4 @@ LearnplaceAspectKernel::getInstance()
 		// Include paths restricts the directories where aspects should be applied, or empty for all source files
 		'includePaths' => [__DIR__]
 	]);
+ */
