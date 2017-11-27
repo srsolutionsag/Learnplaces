c4fc835dc036bfb3539ee298db86ad418d7d2960
Nicolas Schäfli <ns@studer-raimann.ch>
2017-11-27 10:29:12 +0100

    Add location service

---
+++ classes/service/publicapi/block/LocationService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace SRAG\Learnplaces\service\publicapi\block;
+
+use InvalidArgumentException;
+use SRAG\Learnplaces\service\publicapi\model\LocationModel;
+
+/**
+ * Interface LocationService
+ *
+ * This interface defines the public available operations which must be
+ * used to manipulate the location of the learnplace.
+ *
+ * @package SRAG\Learnplaces\service\publicapi\block
+ *
+ * @author  Nicolas Schäfli <ns@studer-raimann.ch>
+ */
+interface LocationService {
+
+	/**
+	 * Updates an existing location or creates a new one if the location id was not found.
+	 *
+	 * @param LocationModel $locationModel  The location which should be updated or created.
+	 *
+	 * @return LocationModel                The newly created or updated location.
+	 */
+	public function store(LocationModel $locationModel): LocationModel;
+
+
+	/**
+	 * Deletes the location with the given id.
+	 *
+	 * @param int $id   The id of the location which should be removed.
+	 *
+	 * @return void
+	 *
+	 * @throws InvalidArgumentException
+	 *                  Thrown if the location with the given id was not found.
+	 */
+	public function delete(int $id);
+
+
+	/**
+	 * Searches a location with the given id.
+	 *
+	 * @param int $id           The id of the location which should be found.
+	 *
+	 * @return LocationModel    The location with the given id.
+	 *
+	 * @throws InvalidArgumentException
+	 *                          Thrown if the location with the given id was not found.
+	 */
+	public function find(int $id): LocationModel;
+
+}
---
+++ classes/service/publicapi/block/LocationServiceImpl.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+
+namespace SRAG\Learnplaces\service\publicapi\block;
+
+use InvalidArgumentException;
+use SRAG\Learnplaces\persistence\repository\exception\EntityNotFoundException;
+use SRAG\Learnplaces\service\publicapi\model\LocationModel;
+use SRAG\Lernplaces\persistence\repository\LocationRepository;
+
+/**
+ * Class LocationServiceImpl
+ *
+ * @package SRAG\Learnplaces\service\publicapi\block
+ *
+ * @author  Nicolas Schäfli <ns@studer-raimann.ch>
+ */
+class LocationServiceImpl implements LocationService {
+
+	/**
+	 * @var LocationRepository $locationRepository
+	 */
+	private $locationRepository;
+
+
+	/**
+	 * LocationServiceImpl constructor.
+	 *
+	 * @param LocationRepository $locationRepository
+	 */
+	public function __construct(LocationRepository $locationRepository) { $this->locationRepository = $locationRepository; }
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function store(LocationModel $locationModel): LocationModel {
+		$dto = $this->locationRepository->store($locationModel->toDto());
+		return $dto->toModel();
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function delete(int $id) {
+		try {
+			$this->locationRepository->delete($id);
+		}
+		catch (EntityNotFoundException $ex) {
+			throw new InvalidArgumentException('The location with the given id could not be deleted, because the it was not found.', 0, $ex);
+		}
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function find(int $id): LocationModel {
+		try {
+			$dto = $this->locationRepository->find($id);
+			return $dto->toModel();
+		}
+		catch (EntityNotFoundException $ex) {
+			throw new InvalidArgumentException('The location with the given id does not exist.', 0, $ex);
+		}
+	}
+}
