fb57f3e172d97179b6809943f8be2eaaf88c991b
Nicolas Schäfli <ns@studer-raimann.ch>
2017-12-03 15:29:25 +0100

    Add picture upload service

---
+++ classes/service/publicapi/block/PictureUploadBlockServiceImpl.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace SRAG\Learnplaces\service\publicapi\block;
+
+use InvalidArgumentException;
+use SRAG\Learnplaces\persistence\repository\exception\EntityNotFoundException;
+use SRAG\Learnplaces\persistence\repository\PictureBlockRepository;
+use SRAG\Learnplaces\persistence\repository\PictureUploadBlockRepository;
+use SRAG\Learnplaces\service\publicapi\model\PictureUploadBlockModel;
+
+/**
+ * Class PictureUploadBlockServiceImpl
+ *
+ * @package SRAG\Learnplaces\service\publicapi\block
+ *
+ * @author  Nicolas Schäfli <ns@studer-raimann.ch>
+ */
+class PictureUploadBlockServiceImpl implements PictureUploadBlockService {
+
+	/**
+	 * @var PictureUploadBlockRepository $pictureUploadBlockRepository
+	 */
+	private $pictureUploadBlockRepository;
+
+
+	/**
+	 * PictureBlockServiceImpl constructor.
+	 *
+	 * @param PictureBlockRepository $pictureBlockRepository
+	 */
+	public function __construct(PictureBlockRepository $pictureBlockRepository) { $this->pictureUploadBlockRepository = $pictureBlockRepository; }
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function store(PictureUploadBlockModel $blockModel): PictureUploadBlockModel {
+		$dto = $this->pictureUploadBlockRepository->store($blockModel->toDto());
+		return $dto->toModel();
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function delete(int $id) {
+		try {
+			$this->pictureUploadBlockRepository->delete($id);
+		}
+		catch (EntityNotFoundException $ex) {
+			throw new InvalidArgumentException('The picture upload block with the given id could not be deleted, because the block was not found.', 0, $ex);
+		}
+	}
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function find(int $id): PictureUploadBlockModel {
+		try {
+			$dto = $this->pictureUploadBlockRepository->findByBlockId($id);
+			return $dto->toModel();
+		}
+		catch (EntityNotFoundException $ex) {
+			throw new InvalidArgumentException('The picture upload block with the given id does not exist.', 0, $ex);
+		}
+	}
+}
