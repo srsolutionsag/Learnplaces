f23de5d6bb490fc08f39cb269bb33479709beaf8
Nicolas Schäfli <ns@studer-raimann.ch>
2018-02-15 14:35:48 +0100

    implement uninstallation of plugin

--- classes/class.ilLearnplacesPlugin.php
+++ classes/class.ilLearnplacesPlugin.php
@@ -1,4 +1,8 @@
 <?php
+
+use League\Flysystem\FilesystemInterface;
+use SRAG\Learnplaces\container\PluginContainer;
+
 require_once __DIR__ . '/bootstrap.php';
 
 /**
@@ -38,6 +42,52 @@ final class ilLearnplacesPlugin extends ilRepositoryObjectPlugin {
 
 
 	protected function uninstallCustom() {
-		throw new Exception("Not Implemented yet.");
+		$this->dropDatabase();
+		$this->deleteFiles();
+	}
+
+	private function dropDatabase() {
+		global $DIC;
+		$database = $DIC->database();
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\AccordionBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\AccordionBlockMember::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Answer::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\AudioBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Block::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Comment::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\CommentBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Configuration::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\ExternalStreamBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\FeedbackBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Feedback::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\HorizontalLineBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\ILIASLinkBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Learnplace::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\LearnplaceConstraint::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Location::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\MapBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Picture::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\PictureBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\PictureGalleryEntry::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\PictureUploadBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\RichTextBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\VideoBlock::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\Visibility::returnDbTableName(), false);
+		$database->dropTable(\SRAG\Learnplaces\persistence\entity\VisitJournal::returnDbTableName(), false);
+	}
+
+	private function deleteFiles() {
+		/**
+		 * @var FilesystemInterface $filesystem
+		 */
+		$filesystem = PluginContainer::resolve(FilesystemInterface::class);
+		$directories = $filesystem->listContents(ilUtil::getWebspaceDir());
+
+		$regex = '/\/' . ilLearnplacesPlugin::PLUGIN_ID . '_\d{1,}$/'; // matches for example /xsrl_254
+		foreach ($directories as $directory) {
+			$path = $directory['path'];
+			if(preg_match($regex, $path) === 1)
+				$filesystem->deleteDir($path);
+		}
 	}
 }
